23b19d8b513aa3d6a74c9678349f9dfc00db2e07
hilmizul <netspytux@gmail.com>
2026-05-20 07:46:17 +0700

    dev: create new page stiker

---
+++ pages/stiker/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card mt-2">
+    <div class="card-header fw-bold bg-transparent">
+      <span class="fs-5">STIKER</span>
+      <div class="fw-normal text-muted">Stiker Aset</div>
+    </div>
+
+    <div class="card-body">
+      <FilterBar :role :assets />
+    </div>
+  </div>
+</template>
+
+
+<script setup>
+definePageMeta({
+  middleware: 'auth'
+})
+
+useHead({
+  title: "Stiker — Sarpras SMKN 4 Tasikmalaya.",
+  desc: "Applikasi Inventaris Aset dan Bahan — SMKN 4 Tasikmalaya."
+})
+
+const user = usePbUser()
+const role = user?.user.value.role
+const client = usePbClient()
+const assets = ref([])
+</script>
