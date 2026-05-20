71b97ed4466261cc1fc2d9ad6020720fb509dfd7
hilmizul <netspytux@gmail.com>
2026-05-20 07:45:40 +0700

    dev: idk. just to-do.

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,12 @@
   <div class="card mt-2">
     <div class="card-body">
       <h5>hola, <strong>{{ nama }}</strong>!</h5>
+
+      <ul>
+        <li>statistik assets</li>
+        <li>realtime total belanja tahun ini (Rp)</li>
+        <li>daftar issue</li>
+      </ul>
     </div>
   </div>
 </template>
