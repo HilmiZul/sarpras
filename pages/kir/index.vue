551933b71ee7661b3019e7ba5bc727c34066e7f5
hilmizul <netspytux@gmail.com>
2026-06-11 01:49:13 +0700

    dev: display kode lokasi

--- pages/kir/index.vue
+++ pages/kir/index.vue
@@ -19,7 +19,7 @@
 
       <div v-else class="row justify-content-center">
         <div v-for="(unit, i) in unit_kerja.items" :key="i" class="col-md-4">
-          <NuxtLink class="hand" data-bs-toggle="modal" data-bs-target="#rincian" @click="fetchAssetsByUnitKerja(unit.id, unit.ruangan)">
+          <NuxtLink class="hand" data-bs-toggle="modal" data-bs-target="#rincian" @click="fetchAssetsByUnitKerja(unit.id, unit.ruangan, unit.kode_lokasi)">
             <div class="card mb-4">
               <div class="card-body p-5 fs-3 fw-bold text-muted text-center">{{ unit.ruangan }}</div>
             </div>
@@ -31,8 +31,8 @@
       <div class="modal" id="rincian" tabindex="-1">
         <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-xl">
           <div class="modal-content">
-            <div class="modal-header fw-bold text-muted">
-              Aset &nbsp;<LoadingPlaceholder v-if="isLoadingAsset" :col="2" :n="1" /> <span v-if="nama_unit">{{ nama_unit }}</span>
+            <div class="modal-header fw-bold text-muted fs-5">
+              KIR &nbsp;<LoadingPlaceholder v-if="isLoadingAsset" :col="2" :n="1" /> <span v-if="nama_unit && kode_lokasi_unit">{{ nama_unit }} &#8212; {{ kode_lokasi_unit }}</span>
               <button class="btn-close" label="Close" data-bs-dismiss="modal"></button>
             </div>
 
@@ -45,11 +45,35 @@
               </div>
 
               <div v-else >
-                Total {{ assets?.items?.length }} dari {{ assets?.totalItems }} items
+                <!-- Total {{ assets?.items?.length }} dari {{ assets?.totalItems }} items -->
                 <table class="table">
+                  <thead>
+                    <tr>
+                      <th width="2">NO.</th>
+                      <th>NAMA BARANG</th>
+                      <th>SPESIFIKASI</th>
+                      <th>SUMBER</th>
+                      <th>TAHUN</th>
+                      <th>VOLUME</th>
+                      <th>RUANG</th>
+                      <th>KONDISI</th>
+                    </tr>
+                  </thead>
+
                   <tbody>
                     <tr v-for="(asset, i) in assets?.items" :key="i">
+                      <td>{{ i+1 }}.</td>
                       <td>{{ asset.nama_aset_barang }}</td>
+                      <td>{{ asset.spesifikasi }}</td>
+                      <td>{{ asset.expand.sumber_aset.nama_sumber }}</td>
+                      <td>{{ asset.expand.tahun_pengadaan.tahun }}</td>
+                      <td>{{ asset.volume }} {{ asset.expand.satuan_aset.nama_satuan }}</td>
+                      <td>{{ asset.expand.unit_kerja.ruangan }}</td>
+                      <td>
+                        <span v-if="asset.kondisi == 'B'">Baik</span>
+                        <span v-else-if="asset.kondisi == 'RR'">Rusak Ringan</span>
+                        <span v-else-if="asset.kondisi == 'RB'">Rusak Berat</span>
+                      </td>
                     </tr>
                   </tbody>
                 </table>
@@ -89,6 +113,7 @@ const perPageAset = 30
 
 const unit_kerja = ref()
 const nama_unit = ref()
+const kode_lokasi_unit = ref()
 const assets = ref([])
 
 
@@ -106,17 +131,19 @@ async function fetchUnitKerja() {
 }
 
 
-async function fetchAssetsByUnitKerja(id_unit, ruangan) {
+async function fetchAssetsByUnitKerja(id_unit, ruangan, kode_lokasi) {
   isLoadingAsset.value = true
 
   let res = await client.collection('aset').getList(1, perPageAset, {
     filter: `unit_kerja="${id_unit}"`,
+    expand: `sumber_aset, unit_kerja, tahun_pengadaan, satuan_aset`,
     sort: `-tahun_pengadaan.tahun`
   })
 
   if(res) {
     assets.value = res
     nama_unit.value = ruangan
+    kode_lokasi_unit.value = kode_lokasi
 
     isLoadingAsset.value = false
   }
